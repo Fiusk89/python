a1e93eb1c1577fa1b671e892a7387872815289f4
Fiusk89 <rphfonseca@gmail.com>
2023-10-03 01:20:05 -0300

    atm

---
+++ 21/import pandas as pd.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import zipfile
+import os
+import urllib.request
+import matplotlib.pyplot as plt
+
+urllib.request.urlretrieve(url, file)
+os.path.join(os.getcwd(), file_)
---
+++ 21/keepsimple.py
@@ -0,0 +1,34 @@
+import random as rd
+
+deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10]
+dealer = 0
+player = 0
+print(dealer)
+
+dealer = rd.choice(deck)
+print(dealer)
+dealer = dealer + rd.choice(deck)
+print(dealer)
+
+while player < 21:
+    if dealer <= 17:
+        print('Dealer somou menos do que 17 pontos e vai pegar mais uma carta.')
+        dealer = dealer + rd.choice(deck)
+        print(dealer)
+        if dealer > 21:
+            print('O dealer "estourou" e você ganhou a rodada!')
+            print(dealer)
+        else:    
+            player = rd.choice(deck)
+            print(player)
+            player = player + rd.choice(deck)
+            print(player)
+            resposta = input(f'Sua vez! Você recebeu duas cartas, totalizando {player}, o deseja continuar? ')
+            if resposta == 's':
+                player = player + rd.choice(deck)
+                print(player)
+                resposta1 = input(f'Com mais uma carta, você totalizou {player}, vai continuar?')
+                if resposta1 == 's':
+                    player = player + rd.choice(deck)
+                    print(player)            
+                break
